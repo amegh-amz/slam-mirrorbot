dd9e05ac8dbc9e6a395950cab5924dbfef07ccd7
AMZ <80452444+amegh-amz@users.noreply.github.com>
2021-09-24 10:32:52 +0300

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -45,8 +45,8 @@ def stats(update, context):
 
 def start(update, context):
     buttons = button_build.ButtonMaker()
-    buttons.buildbutton("Repo", "https://github.com/SlamDevs/slam-mirrorbot")
-    buttons.buildbutton("Channel", "https://t.me/SlamMirrorUpdates")
+    buttons.buildbutton("Repo", "https://t.me/tg_bot_support")
+    buttons.buildbutton("Channel", "https://t.me/tg_bot_support")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
@@ -154,9 +154,9 @@ help_string_telegraph = f'''<br>
 <b>/{BotCommands.StatsCommand}</b>: Show Stats of the machine the bot is hosted on
 '''
 help = Telegraph(access_token=telegraph_token).create_page(
-        title='Slam Mirrorbot Help',
-        author_name='Slam Mirrorbot',
-        author_url='https://github.com/SlamDevs/slam-mirrorbot',
+        title='MT MIRROR HELP',
+        author_name='😈 DEVIL 😈',
+        author_url='https://t.me/amzmtaccount',
         html_content=help_string_telegraph,
     )["path"]
 
